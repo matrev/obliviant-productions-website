d2cdde5d19ceee6304d7005aa47a47ca62a8a529
matrev <matrev@DESKTOP-MATREV.localdomain>
2020-10-01 12:24:30 -0500

    added new pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Episodes from './pages/Episodes';
+import { Flex, Link, Box, Image } from 'rebass';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Flex
+        p={3}
+        color="white"
+        bg="primary"
+        alignItems="center"
+      >
+        <Link variant="nav" href="">Home</Link>
+        <Box mx="auto" />
+        <Link variant="nav" href="/About">About</Link>
+        <Link variant="nav" href="/Episodes">Episodes</Link>
+      </Flex>
+        {/* <Link to="/Blog">Blog</Link> */}
+
+        <Route exact path="/" component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Episodes" component={Episodes} />
+      </BrowserRouter>
     </div>
   );
 }
