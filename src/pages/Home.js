f1cb9d1283ff7c660dba2246e7355ca1c76a6e2b
matrev <markanthonytrevinojr@gmail.com>
2020-12-22 11:05:09 -0600

    added RSSfeed reader

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Parser from 'rss-parser';
 
 export default function Home () {
+    const [latestEpisodes, setLatestEpisodes] = useState({});
+    async function getEpisodes() {
+        let parser = new Parser();
+        const feed = await parser.parseURL('https://feeds.simplecast.com/kASbzC1o');
+        setLatestEpisodes({ feed });
+        console.log("feed: ", feed);
+    }
+
+    useEffect(() => {
+        getEpisodes();
+    }, []);
+
     return (
         <div>
             Home
