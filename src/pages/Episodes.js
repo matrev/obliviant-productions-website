8bf8185d2bc5848e4d718bb1e5ac00e7d64557f8
matrev <markanthonytrevinojr@gmail.com>
2020-12-26 22:48:16 -0600

    added search functionality'

--- src/pages/Episodes.js
+++ src/pages/Episodes.js
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import Parser from 'rss-parser';
 import EpisodeCollection from '../components/EpisodeCollection';
+import { Label, Input } from '@rebass/forms';
 
 export default function Episodes () {
 
     const [latestEpisodes, setLatestEpisodes] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
     async function getEpisodes() {
         let parser = new Parser();
         const feed = await parser.parseURL('https://feeds.simplecast.com/kASbzC1o');
-        setLatestEpisodes(feed.items);
+        setLatestEpisodes(feed.items.filter((episode) => {
+            console.log(searchInput);
+            return episode.title.toLowerCase().includes(searchInput.toLowerCase());
+        }));
         console.log("feed: ", feed.items);
     }
 
     useEffect(() => {
         getEpisodes();
-    }, []);
-
+    }, [searchInput]);
+    // console.log(searchInput);
     return (
         <div>
+            <Label htmlFor='search'>Search For an Episode</Label>
+            <Input
+                id='search'
+                name='search'
+                value={searchInput}
+                onChange={(e) => {setSearchInput(e.target.value)}}
+            ></Input>
             <EpisodeCollection episodes={latestEpisodes} />
         </div>
     )
