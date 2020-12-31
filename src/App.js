ebdfa8fb8b7257309a22927836c9b0f47456d648
matrev <markanthonytrevinojr@gmail.com>
2020-12-30 22:51:09 -0600

    added navigation accomodations to mobile users

--- src/App.js
+++ src/App.js
@@ -1,43 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
 import Episodes from './pages/Episodes';
 import { Flex } from 'rebass';
-import { Nav, NavbarBrand, NavLink } from 'reactstrap';
+import { useMediaQuery } from 'react-responsive';
+import { Navbar, Nav, NavbarBrand, NavLink, NavbarToggler, Collapse, NavItem} from 'reactstrap';
 
 function App() {
-  return (
 
-    <div className="App">
-		<BrowserRouter basename={"/"}>
-		<Nav className="header-style">
-			<Flex
-				px={2}
-  				color='white'
- 				// bg='black'
-  				alignItems='center'>
-				<NavbarBrand href={"/"} className="mr-auto" >
-					<img src={require("./assets/logo.PNG")} style={{width: 50}} alt="Navigation bar brand logo"/>
-				</NavbarBrand>
-				<NavLink style={{ color: "white" }} variant="nav" href="/about">About</NavLink>
-				<NavLink style={{ color: "white" }} variant="nav" href="/episodes">Episodes</NavLink>
-			</Flex>
+	const [collapsed, setCollapsed] = useState(true);
+  	const toggleNavbar = () => setCollapsed(!collapsed);
 
-      	</Nav>
-      
-      
-        {/* <Box mx="auto" /> */}
-        
-      	
-        {/* <Link to="/Blog">Blog</Link> */}
+	const isDesktopOrLaptop = useMediaQuery({
+		query: '(min-device-width: 1224px)'
+	});
 
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-		<Route path="/episodes" component={Episodes} />
-      </BrowserRouter>
-    </div>
+	const isTabletOrMobileDevice = useMediaQuery({
+		query: '(max-device-width: 1224px)'
+	});
+
+	return (
+		<div className="App">
+			<BrowserRouter basename={"/"}>
+				{isDesktopOrLaptop && 
+					<Nav className="header-style">
+						<Flex
+							px={2}
+							color='white'
+							// bg='black'
+							alignItems='center'>
+							<NavbarBrand href="/" className="mr-auto" >
+								<img src={require("./assets/logo.PNG")} style={{width: 50}} alt="Navigation bar brand logo"/>
+							</NavbarBrand>
+							<NavLink style={{ color: "white" }} variant="nav" href="/about">About</NavLink>
+							<NavLink style={{ color: "white" }} variant="nav" href="/episodes">Episodes</NavLink>
+						</Flex>
+					</Nav>
+				}
+
+				{isTabletOrMobileDevice && 
+					<Navbar className="header-style" dark>
+						<NavbarBrand href="/" className="mr-auto">
+							<img src={require("./assets/logo.PNG")} style={{width: 50}} alt="Navigation bar brand logo"/>
+						</NavbarBrand>
+						<NavbarToggler onClick={toggleNavbar} className="mr-2" />
+						<Collapse isOpen={!collapsed} navbar>
+						<Nav navbar>
+							<NavItem>
+								<NavLink style={{ color: "white" }} variant="nav" href="/about">About</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink style={{ color: "white" }} variant="nav" href="/episodes">Episodes</NavLink>
+							</NavItem>
+						</Nav>
+						</Collapse>
+					</Navbar>
+				}
+
+				<Route exact path="/" component={Home} />
+				<Route path="/about" component={About} />
+				<Route path="/episodes" component={Episodes} />
+			</BrowserRouter>
+		</div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { ThemeProvider } from 'emotion-theming';
 import theme from '@rebass/preset';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
