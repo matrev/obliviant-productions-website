6bfe18e5ef9ea87ce748e31e60879c630d6a6088
Mark Treviño <32989053+matrev@users.noreply.github.com>
2021-01-31 15:09:04 -0600

    added build folders for automatic deploys on firebase

---
+++ build/precache-manifest.4895cbca2b66eef07a4485e78c27f530.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e202693ef2fb1650f7819ba6f6a237",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ae513777198c9532fa9",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7d95d87cad64b3eaf107",
+    "url": "/static/css/main.a20b392b.chunk.css"
+  },
+  {
+    "revision": "1ae513777198c9532fa9",
+    "url": "/static/js/2.6f7ae7d2.chunk.js"
+  },
+  {
+    "revision": "5a7fd3a5cd16069af32b31989da6d7f0",
+    "url": "/static/js/2.6f7ae7d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d95d87cad64b3eaf107",
+    "url": "/static/js/main.842f1489.chunk.js"
+  },
+  {
+    "revision": "2f00e27ee82fc3407f60",
+    "url": "/static/js/runtime-main.8e50440a.js"
+  },
+  {
+    "revision": "a7228ba460addab5203483a1ed3dc2bf",
+    "url": "/static/media/anthony.a7228ba4.jpg"
+  },
+  {
+    "revision": "f4a7066492266065cedfad12336b64ec",
+    "url": "/static/media/discordIcon.f4a70664.png"
+  },
+  {
+    "revision": "cc82494ea31bc2043711b6619245f143",
+    "url": "/static/media/instaIcon.cc82494e.png"
+  },
+  {
+    "revision": "d434bb4de7b6cbb680a0c1eee1380609",
+    "url": "/static/media/livia.d434bb4d.jpg"
+  },
+  {
+    "revision": "82a8f0c69ba81f9cfc2e774be7bcc8e0",
+    "url": "/static/media/logo.82a8f0c6.png"
+  },
+  {
+    "revision": "d0d7f78423891fc622d41aa0d82630b5",
+    "url": "/static/media/spotifyIcon.d0d7f784.png"
+  }
+]);
