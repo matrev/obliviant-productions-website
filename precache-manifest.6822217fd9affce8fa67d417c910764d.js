af384edecced4b74cb15a08e50c9f8ecae668cab
matrev <markanthonytrevinojr@gmail.com>
2022-10-03 23:10:26 -0700

    Updates

---
+++ precache-manifest.6822217fd9affce8fa67d417c910764d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc49539782300350d14834277a735d87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2d1390b8e63b697ea4e",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "2efd0c39cd4c51ac7b61",
+    "url": "/static/css/main.dd30d293.chunk.css"
+  },
+  {
+    "revision": "b2d1390b8e63b697ea4e",
+    "url": "/static/js/2.3650ecd5.chunk.js"
+  },
+  {
+    "revision": "db94e03f5604d0bb4bc267fa4b4b74f0",
+    "url": "/static/js/2.3650ecd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2efd0c39cd4c51ac7b61",
+    "url": "/static/js/main.85d39f9c.chunk.js"
+  },
+  {
+    "revision": "2f00e27ee82fc3407f60",
+    "url": "/static/js/runtime-main.8e50440a.js"
+  },
+  {
+    "revision": "a7228ba460addab5203483a1ed3dc2bf",
+    "url": "/static/media/anthony.a7228ba4.jpg"
+  },
+  {
+    "revision": "f4a7066492266065cedfad12336b64ec",
+    "url": "/static/media/discordIcon.f4a70664.png"
+  },
+  {
+    "revision": "cc82494ea31bc2043711b6619245f143",
+    "url": "/static/media/instaIcon.cc82494e.png"
+  },
+  {
+    "revision": "d434bb4de7b6cbb680a0c1eee1380609",
+    "url": "/static/media/livia.d434bb4d.jpg"
+  },
+  {
+    "revision": "6b988403b9671264a0c814087aed16d8",
+    "url": "/static/media/logo.6b988403.png"
+  },
+  {
+    "revision": "d0d7f78423891fc622d41aa0d82630b5",
+    "url": "/static/media/spotifyIcon.d0d7f784.png"
+  }
+]);
